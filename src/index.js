f0d61a1a346954a6d7cabb4f986c8f6f10e363bc
Dias.Kaztai <dias.kaztai@softlex.pro>
2020-07-07 12:22:04 +0300

    racket logic end

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './scss/index.scss'
+
+const racket = document.querySelector('.racket')
+
+let left = 35
+
+racket.focus()
+racket.addEventListener('keydown', (event) => {
+    if(event.key === 'ArrowLeft'){
+        left = left - 3
+        event.target.style = `left:${left}%`
+        if(left <= 5){
+            left = 5
+        }
+    }
+    if(event.key === 'ArrowRight'){
+        left = left + 3
+        event.target.style = `left:${left}%`
+    }
+    if(left >= 75){
+        left = 75
+    }
+})

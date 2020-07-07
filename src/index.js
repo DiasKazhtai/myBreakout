dabb6d3abbcdc7daee98909d1759f7471ab4ebd7
Dias.Kaztai <dias.kaztai@softlex.pro>
2020-07-07 12:30:11 +0300

    ball layout end

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ racket.addEventListener('keydown', (event) => {
     if(event.key === 'ArrowLeft'){
         left = left - 3
         event.target.style = `left:${left}%`
-        if(left <= 5){
-            left = 5
+        if(left <= 10){
+            left = 10
         }
     }
     if(event.key === 'ArrowRight'){
         left = left + 3
         event.target.style = `left:${left}%`
     }
-    if(left >= 75){
-        left = 75
+    if(left >= 70){
+        left = 70
     }
 })
